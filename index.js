63b8ca004661463423ed3431ca39a9915d0b323e
Adrienne Miranda <adrienne.c.miranda@gmail.com>
2019-05-19 20:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string){
+  if (string === 'I love you, Grandma.'){
+    return 'I love you, too.'
+  }
+  else if (string === lowercase){
+    return 'I can\'t hear you!'
+  }
+  else if (string === uppercase){
+    return 'YES INDEED!'
+  }
+  
+}
